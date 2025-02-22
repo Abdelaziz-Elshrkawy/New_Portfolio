d0f12237c73b3f4f142e67020f735c352a91a412
abdelaziz-elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2025-02-22 13:58:06 +0200

    resume link update

--- Data/data.js
+++ Data/data.js
@@ -1,169 +1,161 @@
 const data = {
-    "sections": [
-        "home",
-        "skills",
-        "certificates"
-    ],
-    "intro": [
-        "Hi",
-        "I am",
-        "Abdelaziz"
-    ],
-    "contactMe": {
-        "facebook": "https://www.facebook.com/zezo.elsharkawy/",
-        "twitter": "https://twitter.com/abdelziz53/",
-        "linkedIn": "https://www.linkedin.com/in/abdelaziz-elshrkawy/",
-        "github": "https://github.com/Abdelaziz-Elshrkawy",
-        "resume": "1Cnj4DMBweyP3vBdNGVqEkR5fHsN-1kJP",
-        "email": "abdelaziz.abdellatif0@gmail.com"
+  sections: ["home", "skills", "certificates"],
+  intro: ["Hi", "I am", "Abdelaziz"],
+  contactMe: {
+    facebook: "https://www.facebook.com/zezo.elsharkawy/",
+    twitter: "https://twitter.com/abdelziz53/",
+    linkedIn: "https://www.linkedin.com/in/abdelaziz-elshrkawy/",
+    github: "https://github.com/Abdelaziz-Elshrkawy",
+    resume: "1TSg-YlhMY6uxrRI0C2Zc0MhZwx1zuS9l",
+    email: "abdelaziz.abdellatif0@gmail.com",
+  },
+  skills: [
+    {
+      name: "html",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
     },
-    "skills": [
+    {
+      name: "css",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    },
+    {
+      name: "javascript",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    },
+    {
+      name: "typescript",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    },
+    {
+      name: "c",
+      percent: "100%",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg",
+    },
+    {
+      name: "nodejs",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    },
+    {
+      name: "expressjs",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    },
+    {
+      name: "postgresql",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+    },
+    {
+      name: "mongodb",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    },
+    {
+      name: "reactjs",
+      percent: "100%",
+      logo: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    },
+    {
+      name: "react-Native",
+      percent: "100%",
+      logo: "https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg",
+    },
+    {
+      name: "angular",
+      percent: "100%",
+      logo: "https://angular.io/assets/images/logos/angular/angular.svg",
+    },
+    {
+      name: "jasmine",
+      percent: "100%",
+      logo: "https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg",
+    },
+    {
+      name: "Illustrator",
+      percent: "100%",
+      logo: "https://www.adobe.com/content/dam/shared/images/product-icons/svg/illustrator.svg",
+    },
+  ],
+  certificates: [
+    {
+      name: "Challenger",
+      link: "https://drive.google.com/file/d/1hI7BT0vrBByRjzr1RVJrjAZx2h0Cv_B7/view",
+      date: "JAN 2022",
+    },
+    {
+      name: "Advanced Full Stack NanoDegree",
+      link: "https://graduation.udacity.com/confirm/LMVD9N2T",
+      date: "AUG 2022",
+      projects: [
         {
-            "name": "html",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+          name: "image Processing API",
+          link: "https://github.com/Abdelaziz-Elshrkawy/Imag-Processing-API",
         },
         {
-            "name": "css",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+          name: "Store Front API",
+          link: "https://github.com/Abdelaziz-Elshrkawy/Store-Front-API",
         },
         {
-            "name": "javascript",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+          name: "Hosting a Full Stack Application",
+          link: "https://github.com/Abdelaziz-Elshrkawy/Hosting-a-Full-Stack-Application",
+        },
+      ],
+    },
+    {
+      name: "Angular Development Cross-Skilling",
+      link: "https://graduation.udacity.com/confirm/GAG2YEXD",
+      date: "OCT 2022",
+      projects: [
+        {
+          name: "MyStore",
+          link: "https://github.com/Abdelaziz-Elshrkawy/MyStore-Udacity-Angular",
+        },
+      ],
+    },
+    {
+      name: "Professional Front-End Web Development",
+      link: "https://graduation.udacity.com/confirm/LRVLGJCE",
+      date: "DEC 2022",
+      projects: [
+        {
+          name: "Landing Page (Portfolio)",
+          link: "https://github.com/Abdelaziz-Elshrkawy/Portfolio",
         },
         {
-            "name": "typescript",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
+          name: "Weather Journal App",
+          link: "https://github.com/Abdelaziz-Elshrkawy/weather-journal-app",
         },
+      ],
+    },
+    {
+      name: "React Development Cross-Skilling",
+      link: "https://confirm.udacity.com/XGRXWG5Z",
+      date: "JAN 2023",
+      projects: [
         {
-            "name": "c",
-            "percent": "100%",
-            "logo": "https://upload.wikimedia.org/wikipedia/commons/1/18/C_Programming_Language.svg"
+          name: "My Reads",
+          link: "https://github.com/Abdelaziz-Elshrkawy/Udacity-ReactND-My-Reads",
         },
-        {
-            "name": "nodejs",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
-        },
-        {
-            "name": "expressjs",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
-        },
-        {
-            "name": "postgresql",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
-        },
-        {
-            "name": "mongodb",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
-        },
-        {
-            "name": "reactjs",
-            "percent": "100%",
-            "logo": "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-        },
-        {
-            "name": "react-Native",
-            "percent": "100%",
-            "logo": "https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"
-        },
-        {
-            "name": "angular",
-            "percent": "100%",
-            "logo": "https://angular.io/assets/images/logos/angular/angular.svg"
-        },
-        {
-            "name": "jasmine",
-            "percent": "100%",
-            "logo": "https://www.vectorlogo.zone/logos/jasmine/jasmine-icon.svg"
-        },
-        {
-            "name": "Illustrator",
-            "percent": "100%",
-            "logo": "https://www.adobe.com/content/dam/shared/images/product-icons/svg/illustrator.svg"
-        }
-    ],
-    "certificates": [
-        {
-            "name": "Challenger",
-            "link": "https://drive.google.com/file/d/1hI7BT0vrBByRjzr1RVJrjAZx2h0Cv_B7/view",
-            "date": "JAN 2022"
-        },
-        {
-            "name": "Advanced Full Stack NanoDegree",
-            "link": "https://graduation.udacity.com/confirm/LMVD9N2T",
-            "date": "AUG 2022",
-            "projects": [
-                {
-                    "name": "image Processing API",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/Imag-Processing-API"
-                },
-                {
-                    "name": "Store Front API",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/Store-Front-API"
-                },
-                {
-                    "name": "Hosting a Full Stack Application",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/Hosting-a-Full-Stack-Application"
-                }
-            ]
-        },
-        {
-            "name": "Angular Development Cross-Skilling",
-            "link": "https://graduation.udacity.com/confirm/GAG2YEXD",
-            "date": "OCT 2022",
-            "projects": [
-                {
-                    "name": "MyStore",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/MyStore-Udacity-Angular"
-                }
-            ]
-        },
-        {
-            "name": "Professional Front-End Web Development",
-            "link": "https://graduation.udacity.com/confirm/LRVLGJCE",
-            "date": "DEC 2022",
-            "projects": [
-                {
-                    "name": "Landing Page (Portfolio)",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/Portfolio"
-                },
-                {
-                    "name": "Weather Journal App",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/weather-journal-app"
-                }
-            ]
-        },
-        {
-            "name": "React Development Cross-Skilling",
-            "link": "https://confirm.udacity.com/XGRXWG5Z",
-            "date": "JAN 2023",
-            "projects": [
-                {
-                    "name": "My Reads",
-                    "link": "https://github.com/Abdelaziz-Elshrkawy/Udacity-ReactND-My-Reads"
-                }
-            ]
-        }
-    ],
-    "provider": [
-        {
-            "name": "udacity",
-            "logo": "https://auth.udacity.com//static/media/logo.08dc9669.svg",
-            "website": "https://www.udacity.com"
-        },
-        {
-            "name": "egfwd",
-            "logo": "https://benaeducation.com/wp-content/uploads/2021/03/Egypt_fwd_logo-1.png",
-            "website": "https://www.egfwd.com/"
-        }
-    ]
-}
+      ],
+    },
+  ],
+  provider: [
+    {
+      name: "udacity",
+      logo: "https://auth.udacity.com//static/media/logo.08dc9669.svg",
+      website: "https://www.udacity.com",
+    },
+    {
+      name: "egfwd",
+      logo: "https://benaeducation.com/wp-content/uploads/2021/03/Egypt_fwd_logo-1.png",
+      website: "https://www.egfwd.com/",
+    },
+  ],
+};
 
-export default data
+export default data;
